89df0f4fa0d94fb5a4f556588062fed7140cc08e
testastrafy@gmail.com <testastrafy@gmail.com>
2024-11-18 22:36:39 +0000

    last updates

--- models/staging/stg_sales_recrutement.sql
+++ models/staging/stg_sales_recrutement.sql
@@ -1,6 +1,9 @@
 {{
     config(
-        materialized = 'table',
+        materialized = 'incremental',
+        unique_key='surrogate_key',
+        partition_by={"field": "date_date", "data_type": "DATE"},
+        cluster_by=["client_id", "transaction_id", "products_id"], 
         enabled = true,
         persist_docs={"relation": true, "columns": true}
     )
@@ -15,3 +18,7 @@ SELECT
     , turnover
     , qty               AS total_products
 FROM {{ ref('sales_recrutement') }}
+
+{% if is_incremental() %}
+    WHERE date_date > (SELECT MAX(date_date) FROM {{ this }})
+{% endif %}
